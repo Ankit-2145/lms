1643f417ccd7cf7f70a295690fc92a1a119eb166
Ankit-2145 <ankitsharma2615@gmail.com>
2025-03-04 13:32:52 +0530

    style: add sidebar layout

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 }
